1b8d2011b47bed9c68318c9aee9ef57b02f96d5b
itsara <khajer@gmail.com>
2024-01-23 22:41:59 +0700

    add simple ui

---
+++ components/Container.js
@@ -0,0 +1,13 @@
+export default function Container() {
+  return (
+    <>
+      task : <input type="text"></input>
+      <input type="button" value="save"></input>
+
+      <ul>
+        <li>task 1 <a href="#">delete</a></li>
+        <li>task 2 <a href="#">delete</a></li>
+      </ul>
+    </>
+  )
+}
