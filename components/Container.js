4859367712356ca2933cb5bbf4d3b6b694c84bfe
itsara <khajer@gmail.com>
2024-01-29 23:05:43 +0700

    improve checkbox and click to delete text when complete this task

--- components/Container.js
+++ components/Container.js
@@ -5,8 +5,9 @@ export default function Container() {
       <input type="button" value="save"></input>
 
       <ul>
-        <li>task 1 <a href="#">delete</a></li>
-        <li>task 2 <a href="#">delete</a></li>
+        <li><input type="checkbox" checked /><del>task 1 </del><a href="#">delete</a></li>
+        <li><input type="checkbox" />task 2 <a href="#">delete</a></li>
+        <li><input type="checkbox" />task 3 <a href="#">delete</a></li>
       </ul>
     </>
   )
